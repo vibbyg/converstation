a6f550bafbc84dd8778c3c04e85c2329c6905cea
Kun Zhu <zkzhu@uwaterloo.ca>
2024-01-27 12:28:42 -0500

    minor style change

--- src/app/common/component/navbar.tsx
+++ src/app/common/component/navbar.tsx
@@ -3,7 +3,7 @@ import { FaRegSmile } from "react-icons/fa";
 export const NavBar = () => {
     return (
         <nav  className="flex justify-between">
-            <h1>
+            <h1 className="uppercase">
                 CONVERSTATION
             </h1>
             <div className="flex">
