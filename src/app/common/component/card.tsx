78dcea3bc577bb91b59f8632d1f3b72e4440fc4a
Vibhor Gautam <elementswaer@gmail.com>
2024-01-27 15:38:53 -0800

    added background grey cards + donation text in landing page

--- src/app/common/component/card.tsx
+++ src/app/common/component/card.tsx
@@ -1,31 +1,15 @@
 import Link from "next/link";
-import { useState } from "react";
+import { ReactNode } from "react";
 
 export interface ICard {
-	title: string;
-	subtitle: string;
+	children: ReactNode;
 }
 export const Card = (props: ICard) => {
-	const { title, subtitle } = props;
+	const { children } = props;
 	return (
-		<div className="flex rounded-[3.125rem] border-[6px] dark">
-			<div className="m-5 rounded-[3.125rem] border-2 dark">
-				<div className="p-20">
-					<h1 className="font-medium text-7xl pt-10">{title}</h1>
-					<h2 className="text-xl mb-[3.125rem] text-center">{subtitle}</h2>
-					<div className="flex flex-col items-center">
-                    <Link href="/game">
-						<button className="p-3 mb-2 rounded-xl border-2 dark text-2xl">
-							BEGIN NEW CONVO
-						</button>
-                        </Link>
-                        <Link href="/help">
-                        <button className="p-3 rounded-xl border-2 dark text-2xl">
-							HOW DOES THIS WORK?
-						</button>
-                        </Link>
-					</div>
-				</div>
+		<div className="flex rounded-[3.125rem] border-4 dark z-10 bg-[var(--bg)]">
+			<div className="m-5 rounded-[3.125rem] border-2 dark border-[var(--grey)]">
+				{children}
 			</div>
 		</div>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
 import Link from "next/link";
 import { Card } from "./common/component/card";
+import { FaRegSmile } from "react-icons/fa";
+
+interface IHomeButton {
+  buttonText: string;
+  path: string;
+}
+
+const HomeButton: React.FC<IHomeButton> = ({ buttonText, path }) => {
+  return (
+    <Link href={path}>
+      <button className="p-3 mb-2 rounded-xl border-2 border-[var(--grey)] text-2xl tracking-wider">
+        {buttonText}
+      </button>
+    </Link>
+  )
+}
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <Card title="CONVERSTATION" subtitle="catalyze your conversations." />
+    <main className="flex min-h-screen flex-col items-center justify-center relative">
+      <div className="w-[45rem] h-[27rem] absolute rounded-3xl origin-top-left -rotate-[21deg] 
+        translate-y-24 -translate-x-80 bg-[var(--grey)]" />
+      <div className="w-[35rem] h-[27rem] absolute rounded-3xl origin-top-left rotate-[9deg] 
+        translate-y-8 translate-x-80 bg-[var(--grey)]" />
+      <Card>
+        <div className="pr-20 pt-20 pl-20 pb-14">
+          <h1 className="font-medium text-7xl pt-10 tracking-wide">CONVERSTATION</h1>
+          <h2 className="text-lg mb-7 mt-2 text-center">catalyze your conversations.</h2>
+          <div className="flex flex-col items-center">
+            <HomeButton buttonText="BEGIN NEW CONVO" path="/game" />
+            <HomeButton buttonText="HOW DOES THIS WORK?" path="/help" />
+          </div>
+        </div>
+      </Card>
+        <h1 className="flex flex-row items-center gap-4 -mb-24 mt-16">
+          <FaRegSmile />
+          made by vibby + kun, buy us a coffee!
+        </h1>
     </main>
   )
 }
