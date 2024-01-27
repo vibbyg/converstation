227dc5857cc48df339f427627cedd639dd8ade23
Kun Zhu <zkzhu@uwaterloo.ca>
2024-01-27 13:24:40 -0500

    initialize help page

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,22 +1,63 @@
-import Link from "next/link";
+import { ReactNode } from "react";
 import { NavHeader } from "../common/component/navheader"
+
+interface HelpSectionProps {
+  header: string;
+  children: ReactNode;
+}
+
+interface VersionCardsProps {
+  version: string;
+  children: ReactNode;
+}
+
+const SubHeader: React.FC<{text: string}> = ({text}) => {
+  return (
+    <h1 className="font-medium uppercase text-2xl pb-2">
+      {text}
+    </h1>
+  )
+}
+
+const HelpSection: React.FC<HelpSectionProps> = ({header, children}) => {
+  return(
+    <section className="px-5 py-2">
+      <SubHeader text={header}/>
+      {children}
+    </section>
+  )
+}
+
+const VersionCards: React.FC<VersionCardsProps> = ({version, children}) => {
+  return (
+    <div className="border border-solid border-4 rounded-2xl p-5 w-80 h-40">
+      <h3 className="text-l">{version}</h3>
+      {children}
+    </div>
+  )
+}
 
 export default function Home() {
     return (
-      <div className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex min-h-screen flex-col py-10 px-20 gap-5">
         <NavHeader path="/" text="how this works"/>
-        <section>
-          <h3>setup</h3>
-          <p>lorem ipsum</p>
-        </section>
-        <section>
-          <h3>disclaimer</h3>
-          <p>lorem ipsum</p>
-        </section>
-        <section>
-          <h3>version notes</h3>
-          <p>lorem ipsum</p>
-        </section>
+        <HelpSection header="set up">
+          <p className="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit or sit amet, consectetur adipiscior sit amet, consectetur adipiscior sit amet, consectetur adipisci or sit amet, consectetur adipisci or sit amet, consectetur adipisci</p>
+          <p>Select below to learn more about each deck:</p>
+        </HelpSection>
+        <HelpSection header="disclaimer">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit or sit amet, consectetur adipiscior sit amet, consectetur adipiscior sit amet, consectetur adipisci or sit amet, consectetur adipisci or sit amet, consectetur adipisci</p>
+        </HelpSection>
+        <HelpSection header="version notes">
+          <div className="flex flex-row gap-5">
+            <VersionCards version="0.0">
+              <div>work in progress !</div>
+            </VersionCards>
+            <VersionCards version="0.1">
+              <div>HELLO</div>
+            </VersionCards>
+          </div>
+        </HelpSection>
       </div>
     )
 }
