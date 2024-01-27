a3e45087c8b912d383a646b3b69d940300aaedea
Kun Zhu <zkzhu@uwaterloo.ca>
2024-01-27 12:10:05 -0500

    create navbar

---
+++ src/app/common/component/navbar.tsx
@@ -0,0 +1,14 @@
+import { FaRegSmile } from "react-icons/fa";
+
+export const NavBar = () => {
+    return (
+        <nav  className="flex justify-between">
+            <h1>
+                CONVERSTATION
+            </h1>
+            <div className="flex">
+                <FaRegSmile />
+            </div>
+        </nav>
+    )
+}
