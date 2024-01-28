b4f4b0874b4cb58e9377781491f3a48902fdf87f
Vibhor Gautam <elementswaer@gmail.com>
2024-01-27 16:03:17 -0800

    added drop shadow for 3d effect on card

--- src/app/common/component/card.tsx
+++ src/app/common/component/card.tsx
@@ -7,7 +7,7 @@ export interface ICard {
 export const Card = (props: ICard) => {
 	const { children } = props;
 	return (
-		<div className="flex rounded-[3.125rem] border-4 dark z-10 bg-[var(--bg)]">
+		<div className="flex rounded-[3.125rem] border-4 dark z-10 bg-[var(--bg)] shadow-2xl">
 			<div className="m-5 rounded-[3.125rem] border-2 dark border-[var(--grey)]">
 				{children}
 			</div>
