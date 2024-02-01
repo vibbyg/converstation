51e11e721ca5dea632719cdf532e02def71307ee
Kun Zhu <zkzhu@uwaterloo.ca>
2024-01-31 23:45:04 -0500

    vertical height

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-      <div className="flex min-h-screen flex-col items-center p-24">
+      <div className="flex flex-col items-center p-24">
         NEW GAME
       </div>
     )
