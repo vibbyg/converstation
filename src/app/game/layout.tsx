312a6bbb62e8b3efbc03a110c3efb0a0c965d6be
Kun Zhu <zkzhu@uwaterloo.ca>
2024-01-31 23:45:25 -0500

    add help router

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -1,4 +1,6 @@
 import { NavBar } from "../common/component/navbar"
+import Link from "next/link";
+import { FaRegQuestionCircle } from "react-icons/fa";
 
 export default function GameLayout({
     children, 
@@ -6,9 +8,12 @@ export default function GameLayout({
     children: React.ReactNode
   }) {
     return (
-      <section>
+      <section className="h-screen">
         <NavBar/>
         {children}
+        <Link href="/help" className="absolute bottom-5 right-10 text-xl">
+          <FaRegQuestionCircle/>
+        </Link>
       </section>
     )
   }
