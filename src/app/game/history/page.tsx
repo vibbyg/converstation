725e0476316e4548e780b601370b657d3cb2dbb1
Kun Zhu <zkzhu@uwaterloo.ca>
2024-02-09 22:04:58 -0500

    initialize history page

---
+++ src/app/game/history/page.tsx
@@ -0,0 +1,9 @@
+import { NavHeader } from "@/app/common/component/navheader"
+
+export default function PromptsHistory() {
+    return (
+        <div className="flex flex-col py-10 px-20 gap-5">
+            <NavHeader path="/game" text="PREVIOUS QUESTIONS"/>
+        </div>
+    )
+}
