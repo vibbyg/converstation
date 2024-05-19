13d8e0b787c0e856bd3c4e5feb69774ff3850259
Kun Zhu <zkzhu@uwaterloo.ca>
2024-05-19 12:44:15 -0400

    move location

--- src/app/data/gameData.ts
+++ src/app/data/gameData.ts
@@ -1,32 +1,39 @@
 import { IDeck } from "../common/component/deck";
 
-export const gameDecks: IDeck[] = [{
+export const gameDecks: IDeck[] = [
+  {
     deckName: "For the Self",
     deckTagline: "for those heart to hearts with the homies",
     deckColour: "#A5E1C8",
-    deckID: 1
+    deckPosition: 0,
+    deckID: 1,
   },
   {
     deckName: "For the Couples",
     deckTagline: "for those heart to hearts with the homies",
     deckColour: "#FFD8C4",
-    deckID: 2
+    deckPosition: 0,
+    deckID: 2,
   },
   {
     deckName: "Classic - Wild",
     deckTagline: "for those heart to hearts with the homies",
     deckColour: "#FFA87D",
-    deckID: 3
+    deckPosition: 0,
+    deckID: 3,
   },
   {
     deckName: "Classic - Mild",
     deckTagline: "for those heart to hearts with the homies",
     deckColour: "#EE7485",
-    deckID: 4
+    deckPosition: 0,
+    deckID: 4,
   },
   {
     deckName: "For the Homies",
     deckTagline: "for those heart to hearts with the homies",
     deckColour: "#8FB2F6",
-    deckID: 5
-  }]
+    deckPosition: 0,
+    deckID: 5,
+  },
+];
