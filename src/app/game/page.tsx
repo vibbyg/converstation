b4e4b3f034c9718a25e75d74faa62701df701781
Vibhor Gautam <elementswaer@gmail.com>
2024-06-28 12:21:25 -0700

    fixed bug preventing users from moving forward and backwards between cards in game

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -7,13 +7,6 @@ import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { filterQuestions } from "@/services/questionServices";
 
-const Counter: React.FC<{ cur: number; total: number }> = ({ cur, total }) => {
-  return (
-    <div className="text-3xl font-bold mb-10">
-      <span>{cur}</span>/<span className="highlight">{total}</span>
-    </div>
-  );
-};
 
 interface ICard {
   question: string;
@@ -22,84 +15,81 @@ interface ICard {
   }
 }
 
+const defaultCard: ICard = {
+  question: "ready to start?",
+    decks: {
+      colour: "#FFF"
+    }
+}
+
+const Counter: React.FC<{ cur: number; total: number }> = ({ cur, total }) => {
+  return (
+    <div className="text-3xl font-bold mb-10">
+      <span>{cur}</span>/<span className="highlight">{total}</span>
+    </div>
+  );
+};
+
 const generateCards = (questions: ICard[], deckSize: number) => {
   // TODO: right now the cards are just being generated based on randomized index;
   //       deckNames not actually taken into consideration
   let maxSize = questions.length;
-  console.log('maxSize: ', maxSize);
   let cardsIdx: number[] = [];
   while (cardsIdx.length < deckSize) {
     let idx = Math.ceil(Math.random() * maxSize);
     if (cardsIdx.indexOf(idx) === -1) cardsIdx.push(idx);
-    console.log('cardsIdx: ', cardsIdx.length)
   }
   let cards = cardsIdx.map((idx) => questions[idx]);
   return cards;
 };
 
 export default function Game() {
-  console.log('start game');
   const [questions, setQuestions] = useState<ICard[]>([]);
   const searchParams = useSearchParams();
 
   // to access deck size and user selected decks, use these consts
   const deckSize = Number(searchParams.get("deckSize"));
-  console.log('questions: ', questions);
 
   const count = deckSize;
 
-  console.log('does this work?');
   useEffect(() => {
-    console.log('this is now running');
     fetchQuestionsAndRandomize();
-    console.log('this is now finished');
   }, []);
 
   const fetchQuestionsAndRandomize = async () => {
-    console.log('fetchQuestions starting');
     try {
-      console.log('fetchQuestion script running');
       const questionData = await filterQuestions(searchParams.getAll("deckNames"));
       const randomQuestionData = generateCards(questionData || [], deckSize)
       setQuestions(randomQuestionData);
-      console.log('check if this runs every time: ', JSON.stringify(questionData));
     }
     catch (error: any) {
       console.log('Fetched error: ', error.message)
     }
   }
 
-  console.log('what');
 
   const [num, setNum] = useState(0);
-  const [curCard, setCurCard] = useState<ICard>({
-    question: "ready to start?",
-    decks: {
-      colour: "#FFF"
-    }
-  });
-
-  const [prevCard, setPrevCard] = useState<ICard | null>(null);
+  const [curCard, setCurCard] = useState<ICard>(defaultCard);
 
   // TODO: Make prevCards into a list to be displayed in separate page
   // const prevCards: ICard[] = [];
   let idx = num;
   const nextCard = () => {
-    if (idx < count) {
-      setPrevCard(questions[idx - 1]);
+    if (num < count) {
+      setCurCard(questions[num]);
       // prevCards.push(curCard);
-      setCurCard(questions[idx]);
-      idx += 1;
-      setNum(idx);
+      setNum(num + 1);
     }
   };
 
   const previousCard = () => {
-    if (idx > 0 && prevCard) {
-      setCurCard(prevCard); 
-      idx -= 1;
-      setNum(idx);
+    if (num - 1 == 0) {
+      setCurCard(defaultCard)
     }
+    else{
+      setCurCard(questions[num - 2]);
+    }
+    setNum(num - 1);
   };
 
   return (
